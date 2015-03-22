2e2f32cf16839f0d5b52c670c9f1e56236190edd
Caitlyn Nita Goetz <cgoetz@onyx.boisestate.edu>
2015-03-22 13:49:52 -0600

    Started working on it.

--- p4/self-organizing-list/wf.c
+++ p4/self-organizing-list/wf.c
@@ -11,6 +11,17 @@ const int MAX_LINE_LENGTH = 2048;
 const char * delimiters = " 0123456789\t;{}()[].#<>\n\r+-/%*\"^~&=!|:\\?,";
 
 int main(int argc, char *argv[]) {
+	if(argc != 2){
+		printf("Usage: %s", argv[0]);
+		exit(0);
+	}
+	char *nextToken;
+	char *save;
+	char *s;
+	char **token;
+	int numTokens;
 
+	s = (char *) malloc(sizeof(char) * MAX_LINE_LENGTH);
+		
 	return 0;
 }
