9b587a3ee0587f22c0f0e66002295a1f70471ab2
Caitlyn Nita Goetz <cgoetz@onyx.boisestate.edu>
2015-03-21 18:07:24 -0600

    Added WordObj.c to implement the WordObj.h

---
+++ p4/self-organizing-list/WordObj.c
@@ -0,0 +1,27 @@
+#include "WordObj.h"
+
+//creates and returns a pointer to a new word object with the given string and
+//frequency.
+WordObjPtr createWordObj(const char *word, const unisgned long int frequency)
+{
+
+}
+
+//Prints a word object as a string in the following format: word frequency
+char * toString(const void * obj)
+{
+
+}
+
+//Frees the word and then the word object itself
+void freeWordObj(const void * obj)
+{
+
+}
+
+//Compares two word objects by comparing the words stored in the objects and
+//returns a 1 (TRUE) if the first word is equal to the second and a 0 (FALSE). 
+int equals(const void * obj1, const void * obj2)
+{
+
+}
