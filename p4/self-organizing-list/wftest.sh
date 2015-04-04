e45a832b35e51e2d37f4fd260d611d89239314ff
Caitlyn Nita Goetz <cgoetz@node50.boisestate.edu>
2015-04-04 17:02:25 -0600

    Wrote the script.  It works!!

--- p4/self-organizing-list/wftest.sh
+++ p4/self-organizing-list/wftest.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Word find executable
+EXE=./self-organizing-list/wf
+
+RESULT=myresult.out
+DIFFRESULT=myresult.diff
+
+# Clear out any results form last run
+cat /dev/null > $RESULT
+
 case $# in
 0) echo "Usage: wftest.sh {std|self} <textfile>"; exit 1;;
 1) echo "Usage: wftest.sh {std|self} <textfile>"; exit 1;;
@@ -7,11 +16,30 @@ esac
 
 textfile=`basename $2`
 inputdir=sample-input
+outputdir=sample-output
+expected=$outputdir/$textfile.out
 
 
 if test "$1" = "std"
 then
 	# run std list test and compare output
+	time ./self-organizing-list/wf --std-list $inputdir/$textfile | sort -r -k2 -k1 >> $RESULT
 else
 	# run self-organizing list test and compare output
+	time ./self-organizing-list/wf --self-organized-list $inputdir/$textfile | sort -k2r -k1r >> $RESULT
+fi
+
+diff -v $expected $RESULT > $DIFFRESULT
+if [ $? -ne 0 ]
+then
+	echo "Processing complete. Sorting output..."
+	echo "Sorting complete. Comparing output..."
+	echo "Test failed. Check $DIFFRESULT for differences."
+	echo
+else
+	echo "Processing complete. Sorting output.."
+	echo "Sorting complete. Comparing output..."
+	echo "All tests passed!"
+	echo
+	rm $DIFFRESULT
 fi
