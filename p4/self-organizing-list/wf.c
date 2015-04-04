71d740be2b56253e090432d7442181b8d1014a2b
Caitlyn Nita Goetz <cgoetz@node29.boisestate.edu>
2015-04-03 21:50:08 -0600

    Worked on it some.  Still not printing out right.`

--- p4/self-organizing-list/wf.c
+++ p4/self-organizing-list/wf.c
@@ -16,24 +16,24 @@
 const int MAX_LINE_LENGTH = 2048;
 const char * delimiters = " 0123456789\t;{}()[].#<>\n\r+-/%*\"^~&=!|:\\?,";
 
-struct list *list;
 
-char * toLower(char * c){ 
-	int i;
-	for(i = 0; i < strlen(c)-1; i++){
-		if(c[i]>=65 && c[i]<=90)
-			c[i]=c[i]+32;
+char * toLowercase(char * c){ 
+	int i = 0;
+	while(c[i]){
+		c[i] = tolower(c[i]);
+		i++;
 	}
 	return c;
 }
 
 int main(int argc, char *argv[]) {
-	printf("\ninto main\n");
+struct list *list = createList(equals, toString, freeWordObj);
+	//printf("\ninto main\n");
 	if(argc != 3){
 		printf("Usage: %s {--self-organized-list|--std-list} <textfile>\n", argv[0]);
 		exit(0);
 	}
-	printf("after first if\n");
+	//printf("after first if\n");
 	
 	FILE *fin;
 	fin = fopen(argv[2], "r");
@@ -42,44 +42,58 @@ int main(int argc, char *argv[]) {
 		exit(errno);
 	}
 
-	printf("read in file\n");	
+	//printf("read in file\n");	
 
 	//Read line-by line
 	char buffer[MAX_LINE_LENGTH];
 	while(fgets(buffer, sizeof(buffer), fin)){
 		//Break line into words
-		printf("into first while loop\n");
+	//	printf("into first while loop\n");
 		char * nextWord;
 		nextWord = strtok(buffer, delimiters);
 		int frequency = 0;
 		if(strcmp(argv[1], "--std-list") == 0){
-			printf("into std list\n");
+	//		printf("into std list\n");
 			while(nextWord != NULL){
-				printf("into while loop\n");
-				char * lowerWord = toLower(nextWord);
+	//			printf("into while loop\n");
+				char * lowerWord =(char *) (toLowercase(nextWord));
+				printf("here:  %s\n", lowerWord);
 				if(search(list, lowerWord) != NULL){
-					printf("found\n");
 					//increment frequency
 					struct node* temp = search(list, lowerWord);
 					WordObjPtr theWord = (WordObjPtr) temp->obj;
 					frequency = theWord->frequency++;
 					WordObjPtr word = createWordObj(lowerWord, frequency);
 					struct node * nodeWord = createNode(word);
-					addAtFront(list, nodeWord);	
+					addAtFront(list, nodeWord);
+	//				printf("found\n");
 				}else{
-					printf("new\n");
+	//				printf("%s\n", nextWord);
 					frequency = 1;
 					WordObjPtr word = createWordObj(lowerWord, frequency);
 					struct node * nodeWord = createNode(word);
 					addAtFront(list, nodeWord);
+	//				printf("new\n");
 				}
-				
+				nextWord = strtok(NULL, delimiters);
+	//			printf("grabbed new word\n");
 				frequency = 0;
 			}
-		}
-		if(strcmp(argv[1], "--self-organized-list") == 0){
-			while(nextWord != '\0'){
-				char * lowerWord = toLower(nextWord);
+			//perror("out of loop");
+			struct node * current = list->head;
+			while(current){
+				WordObjPtr myword= (WordObjPtr) current->obj;
+				printf("test: %s %lu\n", myword->word, myword->frequency); 
+				char * temp = toString(current->obj);
+				printf("%s\n", temp);
+				free(temp);
+				current = current->next;
+			}
+			free(current);
+
+		}else if(strcmp(argv[1], "--self-organized-list") == 0){
+			while(nextWord != NULL){
+				char * lowerWord = toLowercase(nextWord);
 				if(search(list, lowerWord) != NULL){
 					//increment frequency
 					struct node * temp = search(list, lowerWord);
@@ -95,7 +109,7 @@ int main(int argc, char *argv[]) {
                                         struct node * nodeWord = createNode(word);
                                         addAtFront(list, nodeWord);
 				}
-			nextWord = strtok(buffer, delimiters);
+			nextWord = strtok(NULL, delimiters);
 			frequency = 0;
 			}
 		}
