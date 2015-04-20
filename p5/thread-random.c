8dd4551b949d2d68284e9d67e20bdefd362c56eb
Caitlyn Nita Goetz <cgoetz@node59.boisestate.edu>
2015-04-20 12:38:49 -0600

    Changed a couple of things.

--- p5/thread-random.c
+++ p5/thread-random.c
@@ -14,15 +14,16 @@
 void generateRandomNumbers(long long);  
 double getMilliSeconds();
 
+int count;
 
 /* The main work routine */
-void generateRandomNumbers(long long int count)  
+void generateRandomNumbers(long long int threadId)  
 {
 	long long int i;
 	long int x;
 
 	srandom(RANDOM_SEED);
-    for (i = 0; i < count; i++) {
+    for (i = 0; i < threadId; i++) {
 		x = random();
 		printf("%ld\n", x);
   	}
@@ -31,7 +32,7 @@ void generateRandomNumbers(long long int count)
 
 int main(int argc, char **argv)
 {
-	long long int count;
+	long long int threadId;
 
     double timeStart = 0;
     double timeElapsed = 0;
@@ -40,11 +41,11 @@ int main(int argc, char **argv)
         fprintf(stderr, "Usage: %s <n>\n" ,argv[0]);
         exit(1);
     }
-    sscanf(argv[1],"%lld",&count); /* lld for long long int */
+    sscanf(argv[1],"%lld",&threadId); /* lld for long long int */
 
     timeStart = getMilliSeconds();	//And we are off
-    generateRandomNumbers(count);
-	printf("generated %lld random numbers\n", count);
+    generateRandomNumbers(threadId);
+	printf("generated %lld random numbers\n", threadId);
 
     timeElapsed = getMilliSeconds() - timeStart;
     printf("Elapsed time:  %lf seconds\n",(double)(timeElapsed/1000.0));
