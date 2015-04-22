086ebc2a419071020f7ddcd4abf33495e8513e83
Caitlyn Nita Goetz <cgoetz@onyx.boisestate.edu>
2015-04-21 20:35:21 -0600

    Put some stuff in here.  Really hope it is working.

--- p5/thread-random.c
+++ p5/thread-random.c
@@ -1,54 +1,62 @@
-
+//TURN OFF PRINT STATEMENTS!!!!!!!!!!!!!!!!!!!
 #include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>  // must include stdio  for pvm3.h to compile correctly
 #include <sys/times.h> /* for times system call */
 #include <sys/time.h>  /* for gettimeofday system call */
+#include <pthread.h>
 
 /*#define DEBUG 1*/
 
-#define RANDOM_SEED   12345678 
-
 //The main work routine
-void generateRandomNumbers(long long);  
+void generateRandomNumbers(void *args);  
 double getMilliSeconds();
-
+long long int count;
 
 /* The main work routine */
-void generateRandomNumbers(long long int count)  
+void generateRandomNumbers(void *args)  
 {
 	long long int i;
 	long int x;
 
-	srandom(RANDOM_SEED);
+	//srandom(RANDOM_SEED);
     for (i = 0; i < count; i++) {
-		x = random();
-		printf("%ld\n", x);
+		x = random_r (count, &args);
   	}
+	pthread_exit(NULL);
 }
 
 
 int main(int argc, char **argv)
 {
-	long long int count;
-
     double timeStart = 0;
     double timeElapsed = 0;
 
-  	if (argc < 2) {
-        fprintf(stderr, "Usage: %s <n>\n" ,argv[0]);
-        exit(1);
+    if (argc < 3) {
+    fprintf(stderr, "Usage: thread-random <numberOfRandoms> <numThreads>\n");
+    exit(1);
     }
     sscanf(argv[1],"%lld",&count); /* lld for long long int */
 
     timeStart = getMilliSeconds();	//And we are off
-    generateRandomNumbers(count);
-	printf("generated %lld random numbers\n", count);
+
+	int i;
+	int *value;
+	int n = atoi(argv[1]);
+	pthread_t *tid = (pthread_t *) malloc(sizeof(pthread_t) * n);
+	for(i = 0; i < n; i++){
+		value = (int *) malloc(sizeof(int));
+		*value = i;
+		pthread_create(&tid[i], NULL, generateRanomNumbers(), (void *) value);
+	}
+	for(i = 0; i < n; i++){
+		pthread_join(tid[i], NULL);
+	}
 
     timeElapsed = getMilliSeconds() - timeStart;
     printf("Elapsed time:  %lf seconds\n",(double)(timeElapsed/1000.0));
     fflush(stdout);
 
-    exit(0);
+    return 0;
 }
