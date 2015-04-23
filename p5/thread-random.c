d9249ce130ecdca1320aff4db344f8025d7ba96d
Caitlyn Nita Goetz <cgoetz@onyx.boisestate.edu>
2015-04-22 18:19:27 -0600

    Added some stuff for random_r

--- p5/thread-random.c
+++ p5/thread-random.c
@@ -8,23 +8,28 @@
 #include <pthread.h>
 
 /*#define DEBUG 1*/
+#define BUFSIZE 32
 
 //The main work routine
 void generateRandomNumbers(void *args);  
 double getMilliSeconds();
 long long int count;
 
+
 /* The main work routine */
 void generateRandomNumbers(void *args)  
 {
-	long long int i;
-	long int x;
+	struct random_data *rdata = (struct random_data *) malloc(sizeof(struct random_data));
+	char *statebuf = (char*) malloc(sizeof(char) * BUFSIZE);
 
-	//srandom(RANDOM_SEED);
-    for (i = 0; i < count; i++) {
-		x = random_r (count, &args);
-  	}
-	pthread_exit(NULL);
+	int32_t value;
+	
+	initstate_r(*(int *) threadId, statebuf, BUFSIZE, rdata);
+	int status = random_r (rdata, &value);
+	if(status != 0){
+		perror("random_r");
+		exit(status);
+	}
 }
 
 
@@ -34,21 +39,21 @@ int main(int argc, char **argv)
     double timeElapsed = 0;
 
     if (argc < 3) {
-    fprintf(stderr, "Usage: thread-random <numberOfRandoms> <numThreads>\n");
-    exit(1);
+    	fprintf(stderr, "Usage: thread-random <numberOfRandoms> <numThreads>\n");
+    	exit(1);
     }
     sscanf(argv[1],"%lld",&count); /* lld for long long int */
 
     timeStart = getMilliSeconds();	//And we are off
 
 	int i;
-	int *value;
+	int *value = 0;
 	int n = atoi(argv[1]);
 	pthread_t *tid = (pthread_t *) malloc(sizeof(pthread_t) * n);
 	for(i = 0; i < n; i++){
 		value = (int *) malloc(sizeof(int));
 		*value = i;
-		pthread_create(&tid[i], NULL, generateRanomNumbers(), (void *) value);
+		pthread_create(&tid[i], NULL, generateRandomNumbers, (void *)value);
 	}
 	for(i = 0; i < n; i++){
 		pthread_join(tid[i], NULL);
